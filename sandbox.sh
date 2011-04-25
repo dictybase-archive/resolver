88f122ab5b6096bcc2a8c1274eb3e82fd571c132
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2011-04-25 11:22:56 -0500

    added bash script for sandboxing this through local-lib

--- sandbox.sh
+++ sandbox.sh
@@ -1,11 +1,33 @@
+#!/bin/bash
+
+bundle=bundle
+
+if [ $# -eq 1 ] 
+then
+ if [ "$1" = "-h" ]
+ then
+   echo >&2 usage: $0 [local-lib-folder]
+   echo >&2 ' sets up local lib environment for the given folder'
+   echo >&2 ' default is *bundle* in current directory'
+   exit 0
+  else
+    bundle=$1
+ fi
+fi
+
+
+echo unsetting standard local-lib variables
+
 unset PERL5LIB
 unset PERL_MM_OPT
 unset INSTALL_BASE
 
-echo setting local lib to $PWD/bundle-5.8
-eval $(perl -Mlocal::lib=$PWD/bundle-5.8)
+folder=$PWD/$bundle
+echo setting local lib to $folder
+eval $(perl -Mlocal::lib=$folder)
 
-echo please run '**' cpanm -L bundle --installdeps '.' '**' from this $PWD directory
-echo to install all dependencies
-echo and then deploy through sandbox environment
+echo please run ' **' cpanm -L $folder --installdeps '.' '**' from this $PWD directory
+echo '  to install all dependencies'
+echo '  and then deploy through sandbox environment'
+echo ' ... all is well ....'
 
